5e0235175593663458c9d981c4021bf75e08542d
Gilles Lengy <hello@gilleslengy.dev>
2024-10-12 21:48:45 +0200

    <meta name="mobile-web-app-capable" content="yes">

--- js/main.js
+++ js/main.js
@@ -3,14 +3,4 @@
 //global variables
 window.onload = function () {
     var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
-
-    // Game States
-    game.state.add('Boot', squaresinvasion.Boot);
-    game.state.add('MainMenu', squaresinvasion.MainMenu);
-    game.state.add('Game', squaresinvasion.Game);
-    game.state.add('GameOver', squaresinvasion.GameOver);
-    game.state.add('Preload', squaresinvasion.Preload);
-
-    // Boot
-    game.state.start('Boot');
 };
